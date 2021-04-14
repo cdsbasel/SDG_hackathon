3407205a167a10f299a2263cebec8c71c9c629ce
Dirk Wulff <dwulff@users.noreply.github.com>
2021-04-14 12:04:02 +0200

    ..

---
+++ _site/1_code/sdg_visualizations_GROUPSEUDONYM.R
@@ -0,0 +1,8 @@
+# SDG hackathon
+# Group: GROUP_PSEUDONYM
+
+library(tidyverse)
+
+# read data
+publications <- read_csv('0_data/publications_sdg.csv')
+projects <- read_csv('0_data/projects_sdg.csv')
